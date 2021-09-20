da7830a174430e4b013f94ffbcd208a9587a5366
Marcus <marcusaastum@gmail.com>
2021-09-20 16:15:22 +0200

    Improved timer for OAuth token

--- volume.py
+++ volume.py
@@ -73,9 +73,10 @@ startit = False
 timeout = time.time()
 
 while True:
-
+        #Request new token after the previous has run out
         if time.time() - timeout > 3600:
             ACCESS_TOKEN = input("Input your Spotify OAuth token:\n")
+            timeout = time.time()
         #Reads data from arduino, and assigns it to variables
         everysplit = s.readline().decode("utf-8")
         play = everysplit[0]
